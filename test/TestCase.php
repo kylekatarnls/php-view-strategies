031247b1e4d4c0b88cba01ae66f6c3f0b7e4c9e1
Leko <leko.noor@gmail.com>
2016-02-28 19:23:01 +0900

    test: Set default configuration before test start

--- test/TestCase.php
+++ test/TestCase.php
@@ -2,7 +2,24 @@
 
 namespace Rush\Test;
 
+use Rush\View;
+
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
-
+    public static function setUpBeforeClass()
+    {
+        View::configure([
+            'base_path' => '',
+            'strategies' => [
+                'dwoo'  => '\\Rush\\Strategy\\Dwoo',
+                'foil'  => '\\Rush\\Strategy\\FOIL',
+                'fenom' => '\\Rush\\Strategy\\Fenom',
+                'jade'  => '\\Rush\\Strategy\\Jade',
+                'latte' => '\\Rush\\Strategy\\Latte',
+                'php'   => '\\Rush\\Strategy\\Plates',
+                'tpl'   => '\\Rush\\Strategy\\Smarty',
+                'twig'  => '\\Rush\\Strategy\\Twig',
+            ],
+        ]);
+    }
 }
